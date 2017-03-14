838801d4e583ff52d7a80a89c9fe222b18acc695
Lithtaphone <l.mingmuong@live.fr>
2017-03-14 18:19:36 +0100

    HTML Partie billetterie

--- wp-content/themes/thememusique/page-templates/accueil.php
+++ wp-content/themes/thememusique/page-templates/accueil.php
@@ -7,3 +7,4 @@ get_header(); ?>
 <?php get_template_part( 'template-parts/content-presentation'); ?>
 <?php get_template_part( 'template-parts/content-concert'); ?>
 <?php get_template_part( 'template-parts/content-expo'); ?>
+<?php get_template_part( 'template-parts/content-billetterie'); ?>
---
+++ wp-content/themes/thememusique/template-parts/content-billetterie.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * The default template for displaying content
+ *
+ * Used for both single and index/archive/search.
+ *
+ * @package FoundationPress
+ * @since FoundationPress 1.0.0
+ */
+
+?>
+<div class="billetterie">
+	<img src="" alt="">
+	<h2>Billetterie</h2>
+	<img src="" alt="">
+	<hr>
+	<div class="individuelle">
+		<h3>Individuelle</h3>
+		<hr>
+		<div class="case_noir">
+			<p>17<sup>€</sup></p>
+			<h4>Tarif A</h4>
+		</div>
+		<div class="case_blanc">
+			<p>11<sup>€</sup></p>
+			<h4>Tarif B</h4>
+			<p>proposé aux abonnés du Théâtre de la Manufacture (Nancy), Nest (Thionville), Caméo (Nancy et Metz), Théâtre de Bussang, La Méridienne (Lunéville), Trait d’Union (Neufchateau), TGP (Frouard, CCN-Ballet de Lorraine, Théâtre du Saulcy (Metz), Arsenal (Metz), ACB (Bar-le-Duc).</p>
+		</div>
+		<div class="case_noir">
+			<p>7<sup>€</sup></p>
+			<h4>Tarif C</h4>
+			<p>proposé aux adhérents de l’AUCCAM, associations et MJC de Vandœuvre, séniors, familles nombreuses, adhérents MGEN demandeurs d’emploi, C.E. partenaires du CCAM, groupes à partir de 5 personnes, adhérents Autre Canal (Nancy).</p>
+		</div>
+		<div class="case_blanc">
+			<p>4<sup>€</sup></p>
+			<h4>Tarif D</h4>
+			<p>proposé aux bénéficiares des minima sociaux, -18 ans, adhérents MGEL, étudiants non salariés.</p>
+		</div>
+		<div class="case_noir">
+			<p>4<sup>€</sup>/6<sup>€</sup></p>
+			<h4>Tarifs studio</h4>
+			<p>pproposé aux adhérents de l’AUCCAM, associations et MJC de Vandœuvre, +60 ans, familles nombreuses, demandeurs d’emploi, étudiants non salariés, C.E. partenaires du CCAM, groupes à partir de 5 personnes, bénéficiaires des minima sociaux, -18 ans, adhérents MGEL et MGEN.</p>
+		</div>
+		<div class="case_blanc">
+			<p>11<sup>€</sup></p>
+			<h4>Tarifs TGP <br> Frouard plein</h4>
+		</div>
+		<div class="case_noir">
+			<p>7<sup>€</sup></p>
+			<h4>Tarifs TGP <br> Frouard réduit</h4>
+		</div>
+		<div class="case_blanc">
+			<p>4<sup>€</sup></p>
+			<h4>Tarifs TGP <br> Frouard RSA</h4>
+		</div>
+	</div>
+	<div class="abonnement">
+		<hr>
+		<h3>Abonnement</h3>
+		<div class="case_noir">
+			<p>60<sup>€</sup></p>
+			<h4>Pass Week-end</h4>
+			<p>individuel et valable pour tous les concerts du mer 24 au dim 28 mai.</p>
+		</div>
+		<div class="case_blanc">
+			<p>54<sup>€</sup></p>
+			<h4>Pass 6</h4>
+			<p>individuel et valable pour 6 concerts au choix sur l’ensemble du festival </p>
+		</div>
+		<div class="case_noir">
+			<p>39<sup>€</sup></p>
+			<h4>Pass 6 réduit</h4>
+			<p>individuel et valable pour 6 concerts au choix sur l’ensemble du festival / proposé aux adhérents de l’AUCCAM, associations et MJC de Vandœuvre, +60 ans, familles nombreuses, demandeurs d’emploi, étudiants non salariés, adhérents MGEL et MGEN, étudiants venant à deux, C.E. partenaires du CCAM, groupes à partir de 5 personnes.</p>
+		</div>
+		<div class="case_blanc">
+			<p>24<sup>€</sup></p>
+			<h4>Pass AUCCAM</h4>
+			<p>individuel et valable pour 4 concerts au choix sur l’ensemble du festival.</p>
+		</div>
+	</div>
+	<p>Pour toute réservation :</p>
+	<button class="hollow button" href="#">www.centremalraux.com</button>
+</div>
